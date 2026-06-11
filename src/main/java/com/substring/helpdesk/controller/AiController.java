b2dd432f37e4a141636af4f0a0eedb5967a8d89f
Priyanshu1844 <priyanshu784563@gmail.com>
2026-06-11 10:19:40 +0530

    new changes

--- src/main/java/com/substring/helpdesk/controller/AiController.java
+++ src/main/java/com/substring/helpdesk/controller/AiController.java
@@ -4,9 +4,10 @@ import com.substring.helpdesk.services.AIService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 
 @RestController
-@RequestMapping("/api/v1/ai")
+@RequestMapping("/helpdesk")
 @RequiredArgsConstructor
 public class AiController {
 
@@ -17,6 +18,10 @@ public class AiController {
             @RequestBody String query, @RequestHeader("ConversationId") String conversationId
     ){
         return ResponseEntity.ok(service.getResponseFromAssistant(query,conversationId));
+    }
+    @PostMapping(value = "/stream")
+    public Flux<String> streamResponse(@RequestBody  String query, @RequestHeader("ConversationId") String conversationId){
+        return this.service.streamResponseFromAssistant(query,conversationId) ;
     }
 
 }
--- src/main/java/com/substring/helpdesk/services/AIService.java
+++ src/main/java/com/substring/helpdesk/services/AIService.java
@@ -12,6 +12,7 @@ import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 
 @Service
 @Getter
@@ -38,4 +39,19 @@ public class AIService {
                 .call()
                 .content();
     }
+
+    public Flux<String> streamResponseFromAssistant(String query, String conversationId) {
+
+
+        return this.chatClient
+                .prompt()
+                .advisors(advisorSpec -> advisorSpec.param(ChatMemory.CONVERSATION_ID, conversationId))
+                //tool informations
+                .tools(ticketDatabaseTool, emailTool)
+                .system(systemPromptResource)
+                .user(query)
+                .stream().content();
+
+    }
+
 }
